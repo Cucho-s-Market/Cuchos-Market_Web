3e866b6ed6f3273ea09ed4ac5c0178d30ee3a777
DIEGO <diego@DESKTOP-ESQLN0C.localdomain>
2023-05-17 23:14:25 -0300

    estilos link

---
+++ src/lib/components/utils/Link.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let href = '';
+    export let text = '';
+    export let type = 'link-primary';
+</script>
+
+<a class="link {type}" href="{href}" target="_blank">{text}</a>
+
+
