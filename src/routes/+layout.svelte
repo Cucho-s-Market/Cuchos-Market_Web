f21f7ec89d8273734454ceb583cf73616bd53c82
aspisano888 <as.pisano27@gmail.com>
2023-05-18 12:55:55 -0300

    regitration v1.1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,6 @@
 <script>
     import "../app.css";
-    import CatalogoHeader from '$lib/components/catalogo/CatalogoHeader.svelte';
-	import CatalogoSidebar from "$lib/components/catalogo/CatalogoSidebar.svelte";
-    import Registration from "$lib/components/forms/registration/Registration.svelte";
 
 </script>
-<div>
-    <CatalogoHeader/>
-</div>
-<p>para registrarte has</p>
-<a href="$/lib/components/forms/registration/Registration.svelte"> clic aqui</a>
+
 <slot />
