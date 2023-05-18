369a914e1de145eb907677234a316d5d544e06d3
enjjel <asmcardozoo@gmail.com>
2023-05-17 23:15:20 -0300

    Componente Boton

--- src/lib/components/utils/Button.svelte
+++ src/lib/components/utils/Button.svelte
@@ -1,8 +1,6 @@
 <script>
     export let text = '';
     export let type = 'btn-primary';
-
-    
 </script>
 
-<button class="btn {type}">{text}</button>
+<button class="btn {type}">{text}</button> 
