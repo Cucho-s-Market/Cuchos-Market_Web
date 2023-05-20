b866557299e03b519543b7050d1ebe8a3eee9b05
angelsebamc <angel.marquez@fenicio.io>
2023-05-20 15:21:17 -0300

    Creando componentes ProductCard y Quantity

---
+++ src/lib/components/products/Product.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Button from "../utils/Button.svelte";
+	import Quantity from "../utils/Quantity.svelte";
+
+    let showQty = false;
+
+    export let data = {
+        name: "Papel Higiénico ELITE Hoja Simple 12 Unidades x 30m",
+        purchasePrice: 2000,
+        listPrice: 1500,
+        discount: 20,
+        iso: "UYU",
+        image: "https://dummyimage.com/200/000/fff"
+    };
+
+    //methods
+    const showQuantity = () => {
+        showQty = !showQty;
+    }
+
+</script>
+
+<div class="card w-[250px] round-[10px] p-5 border border-br-grey bg-base-100 font-poppins">
+    <img class="round-[5px] pb-5" src="{data.image}" alt="{data.name}" />
+    <div class="card-body p-0 gap-5 justify-center">
+        <h2 class="flex justify-center font-medium text-sm text-center">{data.name}</h2>
+        <div class="flex flex-col justify-center">
+            <div class="flex flex-col text-center">
+                <p class="font-regular text-xs text-normal-grey font-medium line-through flex-grow-0">{data.iso} {data.purchasePrice}</p>
+                <p class="font-regular text-lg font-medium flex-grow-0">{data.iso} {data.listPrice}</p>
+            </div>
+
+            {#if data.discount > 0}
+                 <div class="flex justify-center">
+                     <p class="contents font-regular text-xs font-medium text-dark-green">{data.discount}% off</p>
+                 </div>
+            {/if}
+        </div>
+        <div class="card-actions justify-center">
+            {#if showQty}
+                <Quantity bind:showQty={showQty}/>
+            {:else}
+                <Button text="Agregar al carrito" type="btn-primary btn-block" click={() => {showQuantity()}}/>
+            {/if}
+        </div>
+    </div>
+</div>
---
+++ src/lib/components/utils/Quantity.svelte
@@ -0,0 +1,45 @@
+<script>
+	import Button from "./Button.svelte";
+
+  export let showQty = true;
+
+  let value = 1;
+
+  let showPlus = false;
+  let showMinus = false;
+
+
+  //events
+  const modifyQty = (btn) => {
+    switch (btn) {
+      case 'plus':
+          value++;
+        break;
+      case 'minus':
+      if(value > 1) {
+          value--;
+        }
+        break;
+    }
+  }
+
+  const addToCart = () => {
+    if(value < 0) {
+      value = 1;
+    }
+
+    alert(value);
+    showQty = false;
+  }
+
+</script>
+
+
+<Button type="btn-error  btn-sm absolute left-5 bottom-20 rounded w-[25%] active:transform-none" svg="shopping-cart-x" click={() => {showQty = false}}/>
+<Button type="btn-success btn-sm absolute right-5 bottom-20 rounded w-[25%] active:transform-none" svg="shopping-cart-plus" click={()=> {addToCart()}}/>
+
+<div class="flex flex-row justify-between w-full">
+		<Button type="btn-primary rounded-none rounded-l w-[25%] active:transform-none" svg="minus" click={()=> {modifyQty('minus')}}/>
+		<input type="number" class="input input-bordered text-center rounded-none w-[50%]"  bind:value="{value}"/>
+		<Button type="btn-primary rounded-none rounded-r w-[25%] active:transform-none" svg="plus" click={() => {modifyQty('plus')}}/>
+</div>
