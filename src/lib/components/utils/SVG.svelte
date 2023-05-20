c50c7735232e2c85e4c66d0e663c7d2586e0a08a
Angel <58599478+angelsebamc@users.noreply.github.com>
2023-05-20 15:02:03 -0300

    Basic structure (#2)
    * Agrego Poppins
    
    * Modificaciones
    
    - SVG: Agrego iconos
    - Button: Agrego soporte para svg
    - app: Agrego font medium - 500
    - tailwind: agrego colores
    
    ---------
    
    Co-authored-by: angelsebamc <angel.marquez@fenicio.io>

--- src/lib/components/utils/SVG.svelte
+++ src/lib/components/utils/SVG.svelte
@@ -28,4 +28,34 @@
         <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
         <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
     </svg>
+{:else if name == "plus"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 5l0 14" />
+        <path d="M5 12l14 0" />
+    </svg>
+{:else if name == "minus"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M5 12l14 0" />
+    </svg>
+{:else if name == "shopping-cart-plus"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        <path d="M17 17h-11v-14h-2" />
+        <path d="M6 5l6 .429m7.138 6.573l-.143 1h-13" />
+        <path d="M15 6h6m-3 -3v6" />
+    </svg>
+{:else if name == "shopping-cart-x"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-x" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        <path d="M17 17h-11v-14h-2" />
+        <path d="M6 5l8 .571m5.43 4.43l-.429 3h-13" />
+        <path d="M17 3l4 4" />
+        <path d="M21 3l-4 4" />
+    </svg>
 {/if}
