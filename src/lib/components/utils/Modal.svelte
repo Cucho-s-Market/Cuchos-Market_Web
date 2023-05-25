64cb8540eca5ebf857ab92b37f5f97461c9d53e8
Shifter24 <felipenahuelviera@hotmail.com>
2023-05-25 11:35:09 -0300

    Agregando disenio de modal, asi como funcionalidad

---
+++ src/lib/components/utils/Modal.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Svg from './SVG.svelte';
+    import { onMount } from 'svelte';
+
+	// @ts-ignore
+	let modalOutside;
+	export let showModal = false;
+
+    //Only when modal is open hide scrollbar in order to not scroll
+    $:if(showModal) document.body.style.overflow = 'hidden';
+
+    function closeModal() {
+        showModal = false;
+        document.body.style.overflow = 'auto'; // Reset body overflow
+    }
+</script>
+
+{#if showModal}
+	<label bind:this={modalOutside} for="my-modal-4" class="modal opacity-100 flex visible pointer-events-auto">
+		<label class="modal-box relative w-[530px] h-[584px] border border-primary" for="">
+			<div class="absolute right-4 cursor-pointer" on:click={closeModal}>
+				<Svg name={'letter-x'} />
+			</div>
+
+			<!-- MAIN CONTENT -->
+            <div class="w-full p-10">
+                
+            </div>
+		</label>
+	</label>
+{/if}
