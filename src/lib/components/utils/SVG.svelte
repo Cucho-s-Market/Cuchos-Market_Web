d129eaa21ef56d2434e69639cc1aa3d468e9a376
Shifter24 <felipenahuelviera@hotmail.com>
2023-05-25 17:40:03 -0300

    Agregando componente login admin. Modificacion de input y SVG

--- src/lib/components/utils/SVG.svelte
+++ src/lib/components/utils/SVG.svelte
@@ -84,6 +84,7 @@
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M7 4l10 16" />
         <path d="M17 4l-10 16" />
+    </svg>
 {:else if name == "arrow-narrow-right"}
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
