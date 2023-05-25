141093a69d2a0e9181155e00a77095463fc0192e
enjjel <asmcardozoo@gmail.com>
2023-05-25 16:56:39 -0300

    creando Swiper

---
+++ src/lib/components/third-party/Swiper.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { register } from 'swiper/element/bundle';
+	import ProductCard from '../products/ProductCard.svelte';
+    register();
+</script>
+
+<div class="flex w-[600px]">
+    <swiper-container
+    slides-per-view="3"
+    speed="400"
+    >
+    <swiper-slide>
+        <ProductCard/>
+    </swiper-slide>
+
+    <swiper-slide>
+        <ProductCard/>
+    </swiper-slide>
+
+    <swiper-slide>
+        <ProductCard/>
+    </swiper-slide>
+
+    <swiper-slide>
+        <ProductCard/>
+    </swiper-slide>
+
+    <swiper-slide>
+        <ProductCard/>
+    </swiper-slide>
+
+    </swiper-container>
+</div>
