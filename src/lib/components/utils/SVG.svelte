7d1ca556e16b94c5e49ba36dea2b95acebbc5944
enjjel <asmcardozoo@gmail.com>
2023-05-25 23:13:13 -0300

    update svg

--- src/lib/components/utils/SVG.svelte
+++ src/lib/components/utils/SVG.svelte
@@ -87,4 +87,26 @@
         <path d="M15 16l4 -4" />
         <path d="M15 8l4 4" />
     </svg>
+{:else if name == "arrow-narrow-left"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M5 12l14 0" />
+        <path d="M5 12l4 4" />
+        <path d="M5 12l4 -4" />
+    </svg>
+{:else if name == "x"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="{props} icon icon-tabler icon-tabler-x" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M18 6l-12 12" />
+        <path d="M6 6l12 12" />
+    </svg>
+{:else if name == "building-store"}
+<svg xmlns="http://www.w3.org/2000/svg" class="{props} icon icon-tabler icon-tabler-building-store" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3 21l18 0" />
+    <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
+    <path d="M5 21l0 -10.15" />
+    <path d="M19 21l0 -10.15" />
+    <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
+</svg>
 {/if}
