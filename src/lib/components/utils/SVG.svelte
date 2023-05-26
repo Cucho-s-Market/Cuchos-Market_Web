becae3f3d0597bc8825288a213db3411606108ee
Angel <58599478+angelsebamc@users.noreply.github.com>
2023-05-25 23:31:40 -0300

    Componente swiper (#20)
    * creando Swiper
    
    * Logica de componente
    
    * update svg

--- src/lib/components/utils/SVG.svelte
+++ src/lib/components/utils/SVG.svelte
@@ -87,6 +87,13 @@
         <path d="M15 16l4 -4" />
         <path d="M15 8l4 4" />
     </svg>
+{:else if name == "arrow-narrow-left"}
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M5 12l14 0" />
+        <path d="M5 12l4 4" />
+        <path d="M5 12l4 -4" />
+    </svg>
 {:else if name == "x"}
     <svg xmlns="http://www.w3.org/2000/svg" class="{props} icon icon-tabler icon-tabler-x" width="{size}" height="{size}" viewBox="0 0 24 24" stroke-width="{stroke}" stroke="{color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
