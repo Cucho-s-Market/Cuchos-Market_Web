f70acdd6eea980ffd59cf6acb9c0d43d519ac5bc
enjjel <asmcardozoo@gmail.com>
2023-05-26 16:55:17 -0300

    Cambios a componetes de Cart

--- src/lib/components/cart/CartFooter.svelte
+++ src/lib/components/cart/CartFooter.svelte
@@ -7,6 +7,6 @@
 <div class="flex justify-center border border-br-grey">
     <div class="flex flex-col gap-6 m-4">
         <p class="font-semibold">Importe total: UYU {total}</p>
-        <Button text={"Finalizar compra"} type={"normal-case btn-primary w-40 block m-auto text-[16px]"}/>
+        <Button text={"Finalizar compra"} type={"normal-case btn-primary block m-auto text-base"}/>
     </div>
 </div>
