7106101bbe45a9d4c68d6834987256c9bdcc1e51
enjjel <asmcardozoo@gmail.com>
2023-05-25 23:05:51 -0300

    Logica de componente

--- src/lib/components/third-party/Swiper.svelte
+++ src/lib/components/third-party/Swiper.svelte
@@ -1,33 +1,41 @@
 <script>
+// @ts-nocheck
+
     import { register } from 'swiper/element/bundle';
-	import ProductCard from '../products/ProductCard.svelte';
+	import SVG from '../utils/SVG.svelte';
     register();
+
+    export let width = "w-full"
+    export let buttons = false;
+    export let slidesPerView = 1;
+    export let pag = false;
+
+    let swiperContainer;
+    let btnNext;
+    let btnPrev;
+
 </script>
 
-<div class="flex w-[600px]">
+<div class="relative flex {width}">
     <swiper-container
-    slides-per-view="3"
-    speed="400"
+        bind:this={swiperContainer}
+        slides-per-view={slidesPerView}
+        autoplay={{
+            delay: 4000
+        }}
+        pagination={pag ? {clickable: true} : false}
     >
-    <swiper-slide>
-        <ProductCard/>
-    </swiper-slide>
 
-    <swiper-slide>
-        <ProductCard/>
-    </swiper-slide>
-
-    <swiper-slide>
-        <ProductCard/>
-    </swiper-slide>
-
-    <swiper-slide>
-        <ProductCard/>
-    </swiper-slide>
-
-    <swiper-slide>
-        <ProductCard/>
-    </swiper-slide>
-
+        <slot/>
     </swiper-container>
+
+    {#if buttons}
+        <button class="absolute left-[-10px] top-1/2 z-10 bg-primary rounded" bind:this={btnPrev} on:click={() => {swiperContainer.swiper.slidePrev()}}>
+            <SVG name="arrow-narrow-left" color="#FFF"/>
+        </button>
+
+        <button class="absolute right-[-10px] top-1/2 z-10 bg-primary rounded" bind:this={btnNext} on:click={() => {swiperContainer.swiper.slideNext()}}>
+            <SVG name="arrow-narrow-right" color="#FFF"/>
+        </button>
+    {/if}
 </div>
