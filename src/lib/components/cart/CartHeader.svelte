512a976d2a73cd62da961c48593b988f992243a9
Angel <58599478+angelsebamc@users.noreply.github.com>
2023-05-26 16:56:18 -0300

    Cambios a componetes de Cart (#25)

--- src/lib/components/cart/CartHeader.svelte
+++ src/lib/components/cart/CartHeader.svelte
@@ -1,5 +1,7 @@
 <script>
     import Svg from "../utils/SVG.svelte";
+
+    export let showCart = false;
 </script>
 
 <div class="flex items-center justify-between">
@@ -7,7 +9,8 @@
         <Svg name={"shopping-cart"} stroke={2}/>
         <p class="font-bold text-[20px]">Carrito</p>
     </div>
-    <div class="flex items-center gap-4 ">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div on:click={() => { showCart = !showCart }} class="flex items-center gap-4 cursor-pointer">
         <p class="font-semibold text-[16px]">Seguir comprando</p>
         <Svg name={"arrow-narrow-right"} size={25}/>
     </div>
