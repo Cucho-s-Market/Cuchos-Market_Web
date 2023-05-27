ee49a8f182000c587598b00320c363cb5adcb8cf
angelsebamc <angel.marquez@fenicio.io>
2023-05-27 19:15:49 -0300

    Cambios footer

--- src/lib/components/common/Footer.svelte
+++ src/lib/components/common/Footer.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="absolute w-full h-[50px] bg-[#000] flex justify-center items-center bottom-0">
+<div class="w-full h-[50px] bg-[#000] flex justify-center items-center mt-20">
 	<div class="text-white text-base">
 		<p>@ 2023 Cucho's Market</p>
 	</div>
