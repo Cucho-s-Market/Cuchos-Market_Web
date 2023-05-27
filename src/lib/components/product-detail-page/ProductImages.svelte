01e265c5dbc82d338d3ef9f85606e93788e199f7
angelsebamc <angel.marquez@fenicio.io>
2023-05-27 13:03:25 -0300

    update product images

---
+++ src/lib/components/product-detail-page/ProductImages.svelte
@@ -0,0 +1,67 @@
+<script>
+	// @ts-nocheck
+	import { onMount } from "svelte";
+
+	let dummyImage = 'https://dummyimage.com/100/09f.png/fff';
+    
+    
+	let images = [
+        { source: 'https://dummyimage.com/450x450/fff', altInf: 'byb en el sillon' },
+		{ source: 'https://dummyimage.com/440x450/fff', altInf: 'pegandose conitos' },
+		{ source: 'https://dummyimage.com/430x450/fff', altInf: 'calzoncillo' },
+		{ source: 'https://dummyimage.com/420x450/fff', altInf: 'cleaning' }
+	];
+    
+	let imgContent;
+    let imagesArray;
+    let mainImage = images[0].source;
+
+    onMount(() => {
+        imagesArray = [...imgContent.childNodes];
+        imagesArray[0].classList.add('border-primary', 'border-2');
+    });
+
+	//@ts-ignore
+	var selectImg = (event) => {
+		const img = event.target;
+		const imgParent = img.parentNode;
+
+		mainImage = img.src != null ? img.src : dummyImage;
+		imgParent.classList.add('border-primary', 'border-2');
+
+		const nonSelected = imagesArray.filter((element) => element != imgParent);
+
+		nonSelected.forEach((element) => {
+			element.classList.remove('border-primary', 'border-2');
+		});
+	};
+</script>
+
+<div class="w-[40%] gap-4">
+	<div class="block sm:flex gap-4">
+		<!-- side images -->
+		<div bind:this={imgContent} class="flex flex-row sm:flex-col gap-4">
+			{#each images as image}
+				<div class="w-[50px] h-[50px] border rounded-md border-br-grey hover:border-primary hover:border-2 cursor-pointer">
+					<img
+						on:click={selectImg}
+						src={image.source}
+						alt={image.altInf}
+						class="w-full h-full object-cover rounded-md"
+					/>
+				</div>
+			{/each}
+		</div>
+		<!-- Main image -->
+		<div class="gap-2">
+			<div class="w-full rounded-md selected:border-2">
+				<img
+					id="mainImage"
+					src={mainImage}
+					alt="asd"
+					class="border border-br-grey rounded-md h-inherit object-cover w-[450px] h-[385px]"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
