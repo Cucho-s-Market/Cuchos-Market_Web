cb345445f5a9cfae35c37d5db8c47a2b0263524f
angelsebamc <angel.marquez@fenicio.io>
2023-05-27 11:22:02 -0300

    quita import no utilizados

--- src/routes/catalogo/+page.svelte
+++ src/routes/catalogo/+page.svelte
@@ -1,13 +1,8 @@
 <script>
 	import CatalogoHeader from "$lib/components/catalogo/CatalogoHeader.svelte";
 	import CatalogoSidebar from "$lib/components/catalogo/CatalogoSidebar.svelte";
-	import Footer from "$lib/components/common/Footer.svelte";
-	import Header from "$lib/components/common/Header/Header.svelte";
-	import ProductCard from "$lib/components/products/ProductCard.svelte";
 	import ProductCatalogue from "$lib/components/products/ProductCatalogue.svelte";
-	import Swiper from "$lib/components/third-party/Swiper.svelte";
 	import Notifications from "$lib/components/utils/Notifications.svelte";
-	import Svg from "$lib/components/utils/SVG.svelte";
 </script>
 
 <main class="w-[1200px] pt-[160px] m-auto">
