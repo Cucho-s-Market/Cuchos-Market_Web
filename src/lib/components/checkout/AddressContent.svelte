e127fa986a5257517d0198442f4a163a41ca10cc
Shifter24 <felipenahuelviera@hotmail.com>
2023-05-28 20:18:14 -0300

    Agregando componente seleccionador de opciones para checkout y address.

---
+++ src/lib/components/checkout/AddressContent.svelte
@@ -0,0 +1,23 @@
+<script>
+	// @ts-nocheck
+	export let address = null;
+</script>
+
+{#if address}
+	<div class="flex gap-4 max-w-[400px]">
+		<!-- Columns -->
+		<div>
+			<p class="font-medium text-[16px]">Direccion:</p>
+			<p class="font-medium text-[16px]">Nro de Puerta:</p>
+			<p class="font-medium text-[16px]">Esquina:</p>
+			<p class="font-medium text-[16px]">Departamento:</p>
+		</div>
+		<!-- Content -->
+		<div>
+			<p class="font-medium text-[16px] text-neutral-grey">{address.address ? address.address : 'N/A'}</p>
+			<p class="font-medium text-[16px] text-neutral-grey">{address.doorNumber ? address.doorNumber : 'N/A'}</p>
+			<p class="font-medium text-[16px] text-neutral-grey">{address.street ? address.street : 'N/A'}</p>
+			<p class="font-medium text-[16px] text-neutral-grey">{address.city ? address.city : 'N/A'}</p>
+		</div>
+	</div>
+{/if}
