5179c771cfa6007248516d3141ce232d07d44c4d
Shifter24 <felipenahuelviera@hotmail.com>
2023-05-28 22:05:33 -0300

    Agregando cambios en checkout y layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script>
-    import "../app.css";
-	import Header from "$lib/components/common/Header/Header.svelte";
-	import Footer from "$lib/components/common/Footer.svelte";
-	import Notifications from "$lib/components/utils/Notifications.svelte";
+	import '../app.css';
+	import Header from '$lib/components/common/Header/Header.svelte';
+	import Footer from '$lib/components/common/Footer.svelte';
+	import Notifications from '$lib/components/utils/Notifications.svelte';
+	import { getContext } from 'svelte';
+	import { page } from '$app/stores';
+
+	let currentPage;
+	const { subscribe } = page;
+	const unsubscribe = subscribe((value) => {
+		currentPage = value;
+	});
+
 </script>
 
-<Header/>
+{#if currentPage?.route?.id == '/home' || currentPage?.route?.id == '/catalogo'}
+	<Header/>
+{/if}
 <slot />
-<Notifications/>
+<Notifications />
 <!-- <Footer/> -->
