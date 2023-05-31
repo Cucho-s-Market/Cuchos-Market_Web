aed7c640b6220eca3cbce0fac1f0345947dc04f4
enjjel <asmcardozoo@gmail.com>
2023-05-30 21:24:43 -0300

    cambio sales -> ventas

--- src/lib/components/admin/common/SideMenu/SideMenu.svelte
+++ src/lib/components/admin/common/SideMenu/SideMenu.svelte
@@ -5,7 +5,7 @@
 <div class="flex w-80 border-t-2 border-base-300 mt-20 min-h-screen bg-base-100">
 	<ul class="menu p-4 w-full text-base-content">
 		<SideMenuOption text="Estadisticas" svg="chart-bar" />
-		<SideMenuOption href="/admin/sales" text="Ventas" svg="coin" />
+		<SideMenuOption href="/admin/ventas" text="Ventas" svg="coin" />
 		<SideMenuOption text="Productos" svg="shopping-bag" />
 		<SideMenuOption text="Marketing" svg="affiliate" />
 		<SideMenuOption text="Logistica" svg="map-pin" />
