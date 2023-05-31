c27cf480c75be0df63f3b8d7931e4360f2549fa6
Angel <58599478+angelsebamc@users.noreply.github.com>
2023-05-30 23:48:55 -0300

    Actualiza admin layout (#47)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,7 @@
 
 	import '../app.css';
 	import Header from '$lib/components/common/Header/Header.svelte';
-	import Footer from '$lib/components/common/Footer.svelte';
 	import Notifications from '$lib/components/utils/Notifications.svelte';
-	import { getContext } from 'svelte';
 	import { page } from '$app/stores';
 
 	let currentPage;
