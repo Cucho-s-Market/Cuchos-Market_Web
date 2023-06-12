491808599b09e9d3b833014c56d6036f37a40863
enjjel <asmcardozoo@gmail.com>
2023-06-12 19:59:27 -0300

    controles de sesion

--- src/lib/components/product-detail-page/ProductDetails.svelte
+++ src/lib/components/product-detail-page/ProductDetails.svelte
@@ -45,9 +45,9 @@
 			<div class="flex flex-col">
 				<p class="text-xs text-success mb-5">{item.status}</p>
 				<h1 class="text-xl text-neutral mb-0.75">
-					{item.name}
+					{item.code}
 				</h1>
-				<p class="text-xs text-neutral-grey">SKU: {item.code}</p>
+				<p class="text-xs text-neutral-grey">SKU: {item.name}</p>
 			</div>
 
 			<div class="flex flex-col">
