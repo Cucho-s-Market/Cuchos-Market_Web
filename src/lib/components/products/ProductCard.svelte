005c344c7739b3ee958f11f5347beced0aa92b72
Shifter24 <felipenahuelviera@hotmail.com>
2023-06-12 02:18:34 -0300

    Adding products from backend into catalogo

--- src/lib/components/products/ProductCard.svelte
+++ src/lib/components/products/ProductCard.svelte
@@ -13,14 +13,9 @@
         userIsloggedIn = await sessionController.isUserLoggedIn();
     });
 
-    export let item = {
-        name: "",
-        purchasePrice: 0,
-        price: 0,
-        discount: 0,
-        iso: "UYU",
-        image: "https://dummyimage.com/200/000/fff"
-    };
+    let dummyImage = 'https://dummyimage.com/100/09f.png/fff';
+
+    export let item = {};
 
     //methods
     const showQuantity = () => {
@@ -32,23 +27,22 @@
     }
 
 </script>
-
-<a href="/catalogo/{createSlug(item?.name || "")}">
+<a href="/catalogo/{createSlug(item?.code || "")}">
     <div class="card w-[220px] round-[10px] p-3 mb-2 border border-br-grey bg-base-100 font-poppins">
         <div class="flex w-[190px] mb-5 h-[190px]">
-            <img class="rounded-[5px] object-cover" src="{item.image}" alt="{item.name}" />
+            <img class="rounded-[5px] object-cover" src="{item.images?.length > 0 ? item.images[0] : dummyImage}" alt="{item.name}" />
         </div>
         <div class="card-body p-0 gap-5 justify-center">
             <h3 class="flex justify-center font-medium text-sm text-center h-[2em]">{item.name}</h3>
             <div class="flex flex-col justify-center">
                 <div class="flex flex-col text-center">
-                    <p class="font-regular text-xs text-normal-grey font-medium line-through flex-grow-0">{item.iso} {item.price}</p>
-                    <p class="font-regular text-lg font-medium flex-grow-0">{item.iso} {item.price}</p>
+                    <p class="font-regular text-xs text-normal-grey font-medium line-through flex-grow-0">UYU {item.price}</p>
+                    <p class="font-regular text-lg font-medium flex-grow-0">UYU {item.price}</p>
                 </div>
     
                 {#if item.discount > 0}
                      <div class="flex justify-center">
-                         <p class="contents font-regular text-xs font-medium text-dark-green">{item.discount}% off</p>
+                         <p class="contents font-regular text-xs font-medium text-dark-green">{item?.discount ? item?.discount : 'N/A'}% off</p>
                      </div>
                 {/if}
             </div>
--- src/routes/catalogo/+page.svelte
+++ src/routes/catalogo/+page.svelte
@@ -1,52 +1,37 @@
 <script>
-    // @ts-nocheck
-	import CatalogoHeader from "$lib/components/catalogo/CatalogoHeader.svelte";
-	import CatalogoSidebar from "$lib/components/catalogo/CatalogoSidebar.svelte";
-	import ProductCatalogue from "$lib/components/products/ProductCatalogue.svelte";
-	import Notifications from "$lib/components/utils/Notifications.svelte";
+	// @ts-nocheck
+	import CatalogoHeader from '$lib/components/catalogo/CatalogoHeader.svelte';
+	import CatalogoSidebar from '$lib/components/catalogo/CatalogoSidebar.svelte';
+	import ProductCatalogue from '$lib/components/products/ProductCatalogue.svelte';
+	import Notifications from '$lib/components/utils/Notifications.svelte';
+	import { onMount } from 'svelte';
+	import productController from '../../logic/productController';
 
-    const getProducts = async() => {
-        let products = [];
-        
-        const res = await fetch('https://dummyjson.com/products');
-        const data = await res.json();
-        if(data.products) {
-            // @ts-ignore
-            data.products.forEach(element => {
-                products.push({
-                    name: element.title,
-                    price: element.price,
-                    discount: element.discountPercentage,
-                    iso: "UYU",
-                    image: element.images[0],
-                });
-            });
-        }
+	let products = [];
 
-        return products;
-    }
+	onMount(async () => {
+		products = await productController.getProducts();
+        if(products == null || products?.error) return null;
 
-    let productsPromise = getProducts();
-
+        products = products.data;
+	});
 </script>
 
 <main class="w-[1200px] pt-[160px] m-auto">
-    {#await productsPromise then products}
-            <CatalogoHeader qtyItems = {products?.length || 0}/>
+	{#if products.length > 0}
+		<CatalogoHeader qtyItems={products?.length || 0} />
 
-            <div class="flex justify-between pt-[80px]">
-                <!-- Side bar categories -->
-                <sidebar>
-                    <CatalogoSidebar/>
-                </sidebar>
-                <!-- Items catalog -->
-                <div>
-                    <ProductCatalogue products={products}/>
-                </div>
-            </div>
-        
+		<div class="flex justify-between pt-[80px]">
+			<!-- Side bar categories -->
+			<sidebar>
+				<CatalogoSidebar />
+			</sidebar>
+			<!-- Items catalog -->
+			<div>
+				<ProductCatalogue {products} />
+			</div>
+		</div>
 
-        <Notifications/>
-    {/await}
+		<Notifications />
+	{/if}
 </main>
-    
