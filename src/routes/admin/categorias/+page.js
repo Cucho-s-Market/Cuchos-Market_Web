ac2b449e160dba18002944f449afb50aafedeb3d
angelsebamc <angel.marquez@fenicio.io>
2023-06-26 00:16:57 -0300

    Arreglando visualizacion de categorias

--- src/routes/admin/categorias/+page.js
+++ src/routes/admin/categorias/+page.js
@@ -4,7 +4,7 @@ import categoryController from '../../../logic/categoryController';
 /** @type {import('./$types').PageLoad} */
 export async function load() {
 
-    const categories = await categoryController.getCategoriesSelect();
+    const categories = await categoryController.getCategories();
     
     return {categories};
 }
