05167d6414b6e953a360aa47396a345fb1f3ffa8
Angel <58599478+angelsebamc@users.noreply.github.com>
2023-06-26 21:50:07 -0300

    Fix categorias (#73)
    * Arreglando visualizacion de categorias
    
    * arreglo page categorias
    
    * Agrega subcategorias
    
    ---------
    
    Co-authored-by: angelsebamc <angel.marquez@fenicio.io>

--- src/routes/admin/categorias/+page.svelte
+++ src/routes/admin/categorias/+page.svelte
@@ -5,48 +5,43 @@
 	import Button from '$lib/components/utils/Button.svelte';
 	import Input from '$lib/components/utils/Input.svelte';
 	import { notify } from '$lib/components/utils/Notifications.svelte';
+	import { onMount } from 'svelte';
 	import categoryController from '../../../logic/categoryController.js';
 	import { Category } from '../../../logic/dtos/Category.js';
 	import formValidator from '../../../logic/helpers/formValidator.js';
 	import sessionAdminController from '../../../logic/sessionAdminController.js';
+	import { each } from 'svelte/internal';
 
 	export let data;
 
-	let thead = ['Codigo', 'Nombre'];
-	let tbody = [];
-    let category = new Category();
+	let category = new Category();
+	let categories = [];
 
 	const getCategories = async () => {
-		
-		const categories = data.categories;
+		categories = data.categories;
 
 		if (!categories || categories.error) {
 			notify({ type: 'alert-error', text: `Ocurrio un error al cargar los productos` });
 			return;
 		}
-
-		categories.forEach((category) => {
-			tbody.push({
-				id: category.id,
-				row: [category.id, category.name]
-			});
-		});
-
-		console.log(data.categories);
 	};
 
-    let create = async () => {
-		let validationArray = [
-			category.name,
-            category.description,
-		];
+	onMount(async () => {
+		await getCategories();
+	});
 
+	let create = async () => {
+		let validationArray = [category.name, category.description];
 
 		let emptyValues = formValidator.emptyValues(validationArray);
-		
+
 		if (emptyValues) {
 			notify({ type: 'alert-error', text: 'Verifique los campos.' });
 			return;
+		}
+
+		if (category.categoryParent === '-1') {
+			category.categoryParent = null;
 		}
 		const token = await sessionAdminController.getUserToken();
 		const res = await categoryController.addCategory(category, token);
@@ -63,13 +58,10 @@
 
 		notify({ type: 'alert-success', text: res.message });
 		formValidator.clear(validationArray);
-        setTimeout(() => {
-            window.location.href = '/admin/categorias';
-        }, 3000);
+		setTimeout(() => {
+			window.location.href = '/admin/categorias';
+		}, 3000);
 	};
-
-	//hay que cambiar
-	getCategories();
 </script>
 
 <SectionHeader title={'Categorias'} />
@@ -78,8 +70,19 @@
 	<div class="flex w-[200px]">
 		<Input bind:value={category.name} label="Nombre" props="h-10" />
 	</div>
-    <div class="flex w-[200px]">
+	<div class="flex w-[200px]">
 		<Input bind:value={category.description} label="Descripcion" props="h-10" />
+	</div>
+	<div class="flex flex-col w-[200px]">
+		<p class="label">
+			<span class="label-text font-semibold">Categoria Padre</span>
+		</p>
+		<select bind:value={category.categoryParent} class="select select-primary w-full h-1 bg-light-grey focus:border-none">
+			<option value="-1" selected>Sin padre</option>
+			{#each categories as category}
+				<option value="{category.id}">{category.name}</option>
+			{/each}
+		</select>
 	</div>
 	<div class="flex flex-col justify-end">
 		<Button
@@ -92,4 +95,18 @@
 	</div>
 </div>
 
-<SectionTable {thead} {tbody} />
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+{#each categories as item}
+	<div tabindex="0" class="collapse {item?.subcategories.length > 0 ? 'collapse-arrow' : ''} border border-base-300 bg-base-200 max-w-[700px] mt-5 shadow rounded-lg">
+		<div class="flex flex-col justify-center collapse-title text-xs font-medium">{item.name}</div>
+		<div class="flex flex-col">
+			{#if item?.subcategories.length > 0}
+				{#each item?.subcategories as subitem}
+					<div class="collapse-content text-xs">
+						<p>{subitem.name}</p>
+					</div>
+				{/each}
+			{/if}
+		</div>
+	</div>
+{/each}
