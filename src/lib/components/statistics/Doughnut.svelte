4b04e1b7cc4ccf1293a3c0d603bb6289c70deb2b
angelsebamc <angel.marquez@fenicio.io>
2023-07-07 19:16:35 -0300

    updating Doughnut

--- src/lib/components/statistics/Doughnut.svelte
+++ src/lib/components/statistics/Doughnut.svelte
@@ -1,34 +1,94 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
-    import { onMount } from 'svelte';
-    import Chart from 'chart.js/auto';
+	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
+	import chartController from '../../../logic/chartController';
+	import branchController from '../../../logic/branchController';
+	import Utils from '../../../logic/helpers/Utils';
 
+	export let chartData = {
+		labels: ['Red', 'Green', 'Blue'],
+		data: [10, 20, 30]
+	};
 
-    export let chartData;
-  
-    let chart;
-    let chartCanvas;
-  
-    onMount(() => {
-      const ctx = chartCanvas.getContext('2d');
-      chart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-          labels: chartData.labels,
-          datasets: [{
-            data: chartData.data,
-            backgroundColor: ['red', 'green', 'blue'],
-            borderColor: 'white',
-            borderWidth: 2
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false
-        }
-      });
-    });
-  </script>
-  
-  <canvas bind:this={chartCanvas}></canvas>
+	let chart;
+	let chartCanvas;
+	let selectedDate = 1;
+  let start = Utils.getDateNow();
+  let end = start;
+  let branch = null;
+
+	async function setChartData() {
+    let day;
+    let month;
+    let year;
+
+    end = start.split('-');
+
+		switch (Number(selectedDate)) {
+			case 1:
+        end = start;
+				break;
+			case 2:
+        day = end[2] - 7;
+        month = day <= 0 ? end[1] - 1 : end[1];
+        year = month <= 0 ? end[0] - 1 : end[0];
+        end = Utils.getDateNow(new Date(year, month, day));
+				break;
+			case 3:
+        day = end[2] - 30;
+        month = day <= 0 ? end[1] - 1 : end[1];
+        year = month <= 0 ? end[0] - 1 : end[0];
+        end = Utils.getDateNow(new Date(year, month, day));
+				break;
+		}
+
+		let chartData = await chartController.getProducts(branch.id, end, start);
+    debugger;
+	}
+
+	onMount(async () => {
+		branch = await branchController.getSelectedBranch();
+    
+		let chartData = await chartController.getProducts(branch.id, end, start);
+
+		const ctx = chartCanvas.getContext('2d');
+		chart = new Chart(ctx, {
+			type: 'doughnut',
+			data: {
+				labels: chartData.labels,
+				datasets: [
+					{
+						data: chartData.data,
+						backgroundColor: ['red', 'green', 'blue'],
+						borderColor: 'white',
+						borderWidth: 2
+					}
+				]
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false
+			}
+		});
+	});
+</script>
+
+<div class="flex w-full">
+	<div class="flex flex-col w-[300px] h-[400px]">
+		<div class="flex">
+			<select
+				bind:value={selectedDate}
+        on:change={() => setChartData()}
+				class="select select-primary w-full bg-light-grey focus:border-none"
+			>
+				<option disabled selected>Tipo</option>
+				<option selected value="1">Hoy</option>
+				<option selected value="2">Semana</option>
+				<option selected value="3">Mes</option>
+			</select>
+		</div>
+		<canvas bind:this={chartCanvas} />
+	</div>
+</div>
--- src/logic/chartController.js
+++ src/logic/chartController.js
@@ -7,16 +7,20 @@ import sessionAdminController from "./sessionAdminController";
 const chartController = (() => {
 
     async function getProducts(branchId, startDate, endDate) {
-        const token = sessionAdminController.getToken();
+           
+        const token = await sessionAdminController.getUserToken();
 
-        const response = await fetchController.execute(`http://localhost:3000/api/v1/chart/products?branch_id=${branchId}&startDate=${startDate}&endDate=${endDate}`, "GET", null, token, 1);
+        const response = await fetchController.execute(`http://localhost:8080/statistics/products?branch_id=${branchId}&startDate=${startDate}&endDate=${endDate}`, "GET", null, token, 1);
 
+        if(response.error) {
+            return null;
+        }
+        
+        return response.data;
     }
-
-
 	return {
 		getProducts
 	}
 })();
 
-export default sessionController;
+export default chartController;
